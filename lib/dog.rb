5d03f9bd4d95d014860c8949c60758446e220d6e
Nathan Dorazil <nate@Nathans-MacBook-Air.local>
2020-06-11 19:27:16 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+def Dog
+    def initialize(name)
+        @name
+    end
+
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def initialize(breed)
+        @breed
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(breed)
+        @breed = breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+def Person
+    def initialize(name)
+        @name
+    end
+
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def initialize(job)
+        @job
+    end
+    
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+end
